0272e54f4badd294ba08d1c070c97bb787c09c00
sakincse21 <saleheen.sakin@gmail.com>
2025-08-01 23:50:55 +0600

    postman added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -17,7 +17,7 @@ router.patch('/admin/:id', authCheck(IRole.ADMIN, IRole.SUPER_ADMIN), validateRe
 router.get('/', authCheck(IRole.ADMIN, IRole.SUPER_ADMIN) , UserControllers.getAllUsers) //by admin
 router.post('/create',validateRequest(createUserZodSchema), UserControllers.createUser)
 router.get('/me', authCheck(...Object.values(IRole)),UserControllers.getMe) //by anyone
-router.patch('/:id',authCheck(...Object.values(IRole)), validateRequest(updateUserZodSchema), UserControllers.updateUser) //not sure
+router.patch('/:id',authCheck(...Object.values(IRole)), validateRequest(updateUserZodSchema), UserControllers.updateUser) //any user
 router.get('/:id', authCheck(IRole.ADMIN, IRole.SUPER_ADMIN) ,UserControllers.getSingleUser) //by admin
 //delete is not needed. can be done using admin patch.
 router.delete('/:id', authCheck(IRole.ADMIN, IRole.SUPER_ADMIN), UserControllers.deleteUser)
